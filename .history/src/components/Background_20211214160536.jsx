dc65278c4f0541a3d7c451720442e2badde79d0f
Tran_Anh_Duy <anhduy070100@gmail.com>
2021-12-17 20:54:32 +0700

    FE

---
+++ .history/src/components/Background_20211214160536.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Background = () => {
+    return (
+        <div className="banner-wrapper has_background">
+            <img src="" className="img-responsive attachment-1920x447 size-1920x447" alt="img" />
+            
+        </div>
+
+    );
+};
+
+export default Background;
